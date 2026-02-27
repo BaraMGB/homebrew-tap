0c50f288aad2d5095f3d3a4c7b10ebc50e207658
Steffen Baranowsky <BaraMGB@freenet.de>
2026-02-27 21:37:41 +0100

    chore: bump version to 0.1.6

--- jumake.rb
+++ jumake.rb
@@ -1,13 +1,13 @@
 class Jumake < Formula
   desc "CMake project initialization for JUCE"
   homepage "https://github.com/BaraMGB/JuMake"
-  version "0.1.5"
+  version "0.1.6"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/BaraMGB/JuMake/releases/download/v#{version}/jumake-macos-arm64.tar.gz"
-      sha256 "placeholder"
+      sha256 "placeholder" # Update after release
     end
     on_intel do
       url "https://github.com/BaraMGB/JuMake/releases/download/v#{version}/jumake-macos-x64.tar.gz"
